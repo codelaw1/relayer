f045bde62d6399462f195a4d0aee16ebb7a159ce
Jacky <41471869+codelaw1@users.noreply.github.com>
2022-11-20 16:39:31 +0800

    Update quick_start.sh

--- quick_start.sh
+++ quick_start.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 # Go needs to be installed and a proper Go environment needs to be configured
+
 git clone https://github.com/cosmos/relayer.git
-# 官方文档 v2.0.0-rc3 太旧有出入
-cd relayer && git checkout v2.2.0-rc3
+
+# not v2.0.0-rc3 
+cd relayer && git checkout v2.2.0-rc1
+
 echo "making rly need minutes..."
-# make install
+make install
 if command -v rly >/dev/null 2>&1; then
   echo 'relay install success'
 else
   echo 'relay install failed'
   exist 8
 fi 
+
 # reuse channels already exist
+mkdir -p ~/.relayer/config
 curl https://raw.githubusercontent.com/codelaw1/relayer/main/examples/config_GOC.yaml >>~/.relayer/config/config.yaml
 
 # create new relayer account
-WALLET=$(rly keys add provider  | jq .address)
-echo "relayer adddress: $WALLET"
+WALLET=$(rly keys add provider default)
 
-# deposit for relayer account
+echo "relayer wallet should write down: $WALLET"
+ADDR=$(echo $WALLET | jq -r .address)
+SEC=$(echo $WALLET | jq -r .mnemonic)
+
+rly keys restore sputnik default "$SEC"
+
+
 # this may be faild due to facuet limit
-curl https://provider-faucet.goc.earthball.xyz/request?address=$WALLET&chain=provider
-curl http://sputnik-faucet.goc.earthball.xyz/request?address=$WALLET&chain=sputnik
-# curl http://apollo-faucet.goc.earthball.xyz/request?address=$WALLET&chain=apollo
+echo "deposit for relayer account $ADDR"
+curl "https://provider-faucet.goc.earthball.xyz/request?address=${ADDR}&chain=provider"
+echo
+curl "http://sputnik-faucet.goc.earthball.xyz/request?address=${ADDR}&chain=sputnik"
+echo
+# curl "http://apollo-faucet.goc.earthball.xyz/request?address=${ADDR}&chain=apollo"
 
 echo "waiting coins filling to relayer account "
 sleep 30s
